f3cb40b0ce97259ecef1784e01a9c27612693785
ishan249 <249patelishan@gmail.com>
2022-09-20 22:13:16 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,112 @@
+const request = require('request');
+const cheerio = require('cheerio');
+const axios = require('axios');
+const fs = require('fs');
+const express = require('express');
+const bodyParser = require("body-parser");
+const path = require("path");
+const ejs = require("ejs");
+const app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(bodyParser.json());
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/index.html");
+});
+app.listen(4008, function() {
+  console.log("server is listening");
+});
+
+app.post("/showContent", function(req, res) {
+  let subredditName = req.body.initialInput;
+  console.log(subredditName);
+  request(`https://www.reddit.com/${subredditName}/hot/`, function(err, resp, body) {
+    if (!err && resp.statusCode === 200) {
+      var $ = cheerio.load(body);
+
+      // Defination of Arrays storing values
+
+      let links = new Array();
+      let headings = new Array();
+      let upvotes = new Array();
+      let comments = new Array();
+
+      //Loop extracting heading of the post
+
+      $('h3').each(function() {
+        var post = $(this)[0].children[0].data;
+        headings.push(post);
+      });
+
+      // Loop extracting links to the post
+
+      $('.SQnoC3ObvgnGjWt90zD9Z').each(function() {
+        var post = $(this).attr('href');
+        var link = `https://www.reddit.com${post}`;
+        links.push(link);
+      });
+
+      // Loop extracting the upvotes
+
+      $('._3a2ZHWaih05DgAOtvu6cIo').each(function() {
+        var post1 = $(this)[0].children[0].data;
+        upvotes.push(post1);
+      });
+
+
+      // Loop extracting comments
+
+
+      $('.FHCV02u6Cp2zYL0fhQPsO').each(function() {
+        var post1 = $(this)[0].children[0].data;
+        comments.push(post1);
+      });
+
+      // Printing all the values in Array
+      console.log("\nPrinting Heading\n\n");
+
+
+      for (var i = 0; i < headings.length; i++) {
+        console.log(headings[i]);
+      }
+
+
+      console.log("\nPrinting links\n\n");
+
+      for (var i = 0; i < links.length; i++) {
+        console.log(links[i]);
+      }
+
+
+      console.log("\nPrinting number of upvotes\n\n");
+
+      for (var i = 0; i < upvotes.length; i++) {
+        console.log(upvotes[i]);
+      }
+
+      console.log("\nPrinting number of comments\n\n");
+
+      for (var i = 0; i < comments.length; i++) {
+        console.log(comments[i]);
+      }
+
+
+      // rendering array values to showContent.ejs file
+
+      res.render('showContent', {
+        'headingsData': headings,
+        'linksData': links,
+        'upvotesData': upvotes,
+        'commentsData': comments
+      });
+    }
+  });
+});
+
+// Code written by ISHAN PATEL (ishan249)
---
+++ public/js/events.js
@@ -0,0 +1,152 @@
+
+function categoryInput() {
+  var ele = document.getElementById('blur');
+  ele.classList.toggle('active');
+  var elem = document.getElementById('secondPart');
+  elem.classList.toggle('active');
+}
+
+mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+function topFunction() {
+  let divi = document.getElementById('thirdPart');
+  divi.scrollIntoView();
+}
+
+// value sending to server script
+function initialValue1Button() {
+  let txt = document.getElementById('new').innerHTML;
+  var elemen = document.getElementById('initial');
+  elemen.value = txt;
+  var ele = document.getElementById('blur');
+  ele.classList.toggle('active');
+  var elem = document.getElementById('secondPart');
+  elem.classList.toggle('active');
+
+}
+
+function initialValue2Button() {
+  let txt = document.getElementById('new1').innerHTML;
+  var elemen = document.getElementById('initial');
+  elemen.value = txt;
+  var ele = document.getElementById('blur');
+  ele.classList.toggle('active');
+  var elem = document.getElementById('secondPart');
+  elem.classList.toggle('active');
+
+}
+
+function initialValue3Button() {
+  let txt = document.getElementById('new2').innerHTML;
+  var elemen = document.getElementById('initial');
+  elemen.value = txt;
+  var ele = document.getElementById('blur');
+  ele.classList.toggle('active');
+  var elem = document.getElementById('secondPart');
+  elem.classList.toggle('active');
+
+}
+
+function initialValue4Button() {
+  let txt = document.getElementById('new3').innerHTML;
+  var elemen = document.getElementById('initial');
+  elemen.value = txt;
+  var ele = document.getElementById('blur');
+  ele.classList.toggle('active');
+  var elem = document.getElementById('secondPart');
+  elem.classList.toggle('active');
+
+}
+
+function initialValue5Button() {
+  let txt = document.getElementById('new4').innerHTML;
+  var elemen = document.getElementById('initial');
+  elemen.value = txt;
+  var ele = document.getElementById('blur');
+  ele.classList.toggle('active');
+  var elem = document.getElementById('secondPart');
+  elem.classList.toggle('active');
+
+}
+
+function initialValue6Button() {
+  let txt = document.getElementById('new5').innerHTML;
+  var elemen = document.getElementById('initial');
+  elemen.value = txt;
+  var ele = document.getElementById('blur');
+  ele.classList.toggle('active');
+  var elem = document.getElementById('secondPart');
+  elem.classList.toggle('active');
+
+}
+
+function initialValue7Button() {
+  let txt = document.getElementById('new6').innerHTML;
+  var elemen = document.getElementById('initialize');
+  elemen.value = txt;
+}
+
+function initialValue8Button() {
+  let txt = document.getElementById('new7').innerHTML;
+  var elemen = document.getElementById('initialize');
+  elemen.value = txt;
+}
+
+function initialValue9Button() {
+  let txt = document.getElementById('new8').innerHTML;
+  var elemen = document.getElementById('initialize');
+  elemen.value = txt;
+}
+
+function initialValue10Button() {
+  let txt = document.getElementById('new9').innerHTML;
+  var elemen = document.getElementById('initialize');
+  elemen.value = txt;
+}
+
+function initialValue11Button() {
+  let txt = document.getElementById('new10').innerHTML;
+  var elemen = document.getElementById('initialize');
+  elemen.value = txt;
+}
+function initialValue12Button() {
+  let txt = document.getElementById('new11').innerHTML;
+  var elemen = document.getElementById('initialize');
+  elemen.value = txt;
+}
+function initialValue13Button() {
+  let txt = document.getElementById('new12').innerHTML;
+  var elemen = document.getElementById('initialize');
+  elemen.value = txt;
+}
+function initialValue14Button() {
+  let txt = document.getElementById('new13').innerHTML;
+  var elemen = document.getElementById('initialize');
+  elemen.value = txt;
+}
+function initialValue15Button() {
+  let txt = document.getElementById('new14').innerHTML;
+  var elemen = document.getElementById('initialize');
+  elemen.value = txt;
+}
+function initialValue16Button() {
+  let txt = document.getElementById('new15').innerHTML;
+  var elemen = document.getElementById('initialize');
+  elemen.value = txt;
+}
+function initialValue17Button() {
+  let txt = document.getElementById('new16').innerHTML;
+  var elemen = document.getElementById('initialize');
+  elemen.value = txt;
+}
